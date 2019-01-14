aeb8ba5c31efd75a7300f2c6942f20b5115bc1a4
cxdtotsj <cxdtotsj@163.com>
2019-01-14 15:01:49 +0800

    add the commonUser \otherCorpUser

--- depend/auth.go
+++ depend/auth.go
@@ -37,12 +37,12 @@ func Auth(token string) map[string]string {
 
 // CorpAuth return CorpId and header of Auth
 func CorpAuth() (corpID string, auth map[string]string, err error) {
-	id, token, err := corpToken()
+	cid, token, err := corpToken()
 	if err != nil {
 		return "", nil, err
 	}
 	auth = Auth(token)
-	return id, auth, nil
+	return cid, auth, nil
 }
 
 // SuperAuth return header of Auth
@@ -51,6 +51,32 @@ func SuperAuth() (auth map[string]string, err error) {
 	if err != nil {
 		return nil, err
 	}
+	auth = Auth(token)
+	return auth, nil
+}
+
+// CommonAuth common user header of the corp "109777231634510875",uid is "125424776930931207"
+func CommonAuth() (auth map[string]string, err error) {
+	email := "common.user@xdchen.com"
+	password := "12345678"
+	j, err := Login(email, "", password)
+	if err != nil {
+		return nil, err
+	}
+	token, _ := j.Get("token").String()
+	auth = Auth(token)
+	return auth, nil
+}
+
+// OtherAuth rcm user header of the other corp "125425254762820103", uid is "125425255299691015"
+func OtherAuth() (auth map[string]string, err error) {
+	email := "othercorp@xdchen.com"
+	password := "12345678"
+	j, err := Login(email, "", password)
+	if err != nil {
+		return nil, err
+	}
+	token, _ := j.Get("token").String()
 	auth = Auth(token)
 	return auth, nil
 }
--- depend/auth_test.go
+++ depend/auth_test.go
@@ -1,6 +1,7 @@
 package depend
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,12 +14,22 @@ func TestCorpToken(t *testing.T) {
 	}{
 		"109777231634510875", map[string]string{},
 	}
-	cid, token := CorpAuth()
+	cid, token, _ := CorpAuth()
 	assert.Equal(t, want.cid, cid)
 	assert.NotEqual(t, want.token, token)
 }
 
 func TestSuperToken(t *testing.T) {
-	token := SuperAuth()
+	token, _ := SuperAuth()
 	assert.NotEqual(t, map[string]string{}, token)
+}
+
+func TestCommonAuth(t *testing.T) {
+	auth, _ := CommonAuth()
+	fmt.Println(auth)
+}
+
+func TestOtherCorpAuth(t *testing.T) {
+	auth, _ := OtherAuth()
+	fmt.Println(auth)
 }
